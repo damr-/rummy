1de33a908fe702e6cc1900818fe0ae3c653e2bb2
David Weichselbaum <damr-@users.noreply.github.com>
2020-06-02 12:13:52 +0200

    add auto camera adjusting

---
+++ Assets/rummy/Scripts/Cam/CameraAnchor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace rummy.Cam
+{
+
+    public class CameraAnchor : MonoBehaviour
+    {
+        public UnityEvent BecameVisible = new UnityEvent();
+
+        private void OnBecameVisible()
+        {
+            BecameVisible.Invoke();
+        }
+    }
+
+}
---
+++ Assets/rummy/Scripts/Cam/RummyCamera.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace rummy.Cam
+{
+
+    [RequireComponent(typeof(Camera))]
+    public class RummyCamera : MonoBehaviour
+    {
+        private readonly float adjustSpeed = 5f;
+        private Camera cam;
+
+        private void Start()
+        {
+            cam = GetComponent<Camera>();
+        }
+
+        private void Update()
+        {
+            cam.orthographicSize += adjustSpeed * Time.deltaTime;
+        }
+
+        public void AnchorBecameVisible()
+        {
+            enabled = false;
+        }
+    }
+
+}
