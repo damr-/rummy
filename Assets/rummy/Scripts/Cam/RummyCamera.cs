20eb02a8d939b544878b67ed339af6d4000f92d7
David Weichselbaum <damr-@users.noreply.github.com>
2020-06-12 20:12:36 +0200

    adjust UI order, cam speed

--- Assets/rummy/Scripts/Cam/RummyCamera.cs
+++ Assets/rummy/Scripts/Cam/RummyCamera.cs
@@ -6,17 +6,19 @@ namespace rummy.Cam
     [RequireComponent(typeof(Camera))]
     public class RummyCamera : MonoBehaviour
     {
-        private readonly float adjustSpeed = 5f;
+        public float moveSpeed = 15f;
+        public float initialSize = 5f;
         private Camera cam;
 
         private void Start()
         {
             cam = GetComponent<Camera>();
+            cam.orthographicSize = initialSize;
         }
 
         private void Update()
         {
-            cam.orthographicSize += adjustSpeed * Time.deltaTime;
+            cam.orthographicSize += moveSpeed * Time.deltaTime;
         }
 
         public void AnchorBecameVisible()
